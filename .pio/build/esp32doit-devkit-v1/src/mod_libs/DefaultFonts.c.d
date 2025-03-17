4bd8d09bf48ec9422b238a183742bef8455a8b77
ngxx-fus <msnp@outlook.com.vn>
2025-03-17 14:50:08 +0700

    save

---
+++ .pio/build/esp32doit-devkit-v1/src/mod_libs/DefaultFonts.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32doit-devkit-v1/src/mod_libs/DefaultFonts.c.o: \
+ src/mod_libs/DefaultFonts.c \
+ /home/ngxxfus/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
